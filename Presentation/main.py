93450ff4bfd445402527d6fbea699509c8748897
philosolog <philosolog@gmail.com>
2023-12-12 17:11:46 -0700

    update main.py

--- Presentation/main.py
+++ Presentation/main.py
@@ -1,7 +1,7 @@
 from manim import *
 from manim_slides import ThreeDSlide
 
-class lesson(ThreeDSlide):
+class Box(ThreeDSlide):
 	def make_textbox(self, str, box):
 		myBaseTemplate = TexTemplate( # TODO: Make my own preamble...
 			documentclass="\documentclass[preview]{standalone}"
@@ -38,4 +38,11 @@ class lesson(ThreeDSlide):
 		self.play(
 			Wiggle(box)
 		)
-		
+class Cobb_Douglas(ThreeDSlide):
+	def construct(self):
+		self.set_camera_orientation(phi=90*DEGREES, theta=0)
+		box = Prism([2, 3, 2]) # TODO: Animate cardboard cutting.
+		box.set_color(LIGHT_BROWN)
+		self.play(
+			SpinInFromNothing(box, angle=-135*DEGREES)
+		)
