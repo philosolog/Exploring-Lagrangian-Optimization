7a58032d57cf9ab9e96fe5874e7aeed137658ae2
Aaron Wang <philosolog@gmail.com>
2024-02-04 12:05:27 -0700

    update main.py

--- Presentation/main.py
+++ Presentation/main.py
@@ -35,20 +35,27 @@ class Key_Differences(Slide): # *: Between when to apply EVT in 3D vs. LM.
 	def construct(self):
 		pass
 # *: Brennan & Jordan
-	class Cobb_Douglas_Introduction(ThreeDSlide):
-		def construct(self):
-			pass
-	class Cobb_Douglas_Problem(Slide):
-		def construct(self):
-			pass
-	class Cobb_Douglas_Solution(ThreeDSlide):
-		def construct(self):
-			pass
-	class Profit_Problem(Slide):
-		def construct(self):
-			pass
-	class Profit_Solution(Slide):
-		def construct(self):
-			pass
+class test(Slide):
+	def construct(self):
+		# *: 1
+		mcdonalds = SVGMobject("references/mcdonalds.svg").scale_to_fit_width(6).to_edge(LEFT)
+		mcdonalds_text = Tex("McDonald's").move_to(LEFT*2.5)
+		mcdonalds_text.set_color_by_gradient(YELLOW, RED)
+		burger_king = SVGMobject("references/burger_king.svg").scale_to_fit_width(6).to_edge(RIGHT)
+		burger_king_text = Tex("Burger King").move_to(RIGHT*2.5)
+		burger_king_text.set_color_by_gradient(RED, ORANGE)
+
+		self.play(Write(mcdonalds_text))
+		self.play(Write(burger_king_text))
+		# *: 2
+		self.next_slide()
+		self.play(Transform(mcdonalds_text, mcdonalds))
+		self.play(Transform(burger_king_text, burger_king))
+class Cobb_Douglas_Introduction(ThreeDSlide):
+	def construct(self):
+		pass
+class Profit_Problem(Slide):
+	def construct(self):
+		pass
 # *: Kerem & Oliver
-# *: Aaron
+		
